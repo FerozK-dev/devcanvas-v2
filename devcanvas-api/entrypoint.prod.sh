4db274f9625921be9fecd489187649fea22b2a1f
FerozK-dev <ferozkhan.deve@gmail.com>
2025-04-13 18:16:54 +0300

    Update docker files for prod and dev

--- devcanvas-api/entrypoint.prod.sh
+++ devcanvas-api/entrypoint.prod.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
+set -e
+
+# Wait for PostgreSQL
+until nc -z postgres 5432; do
+  echo "Waiting for PostgreSQL..."
+  sleep 1
+done
+
+# Run migrations
 npx prisma migrate deploy
-node dist/index.js
+
+# Start app
+exec node dist/index.js
